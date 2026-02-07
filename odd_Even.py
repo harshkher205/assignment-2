8b1d09d58a6bcfb40fc70b1a5f12813e1dad9aab
harshkher205 <hkher98@gmail.com>
2026-02-07 13:55:56 +0530

    Fix formatting and add newline in odd_Even.py
    Added a newline at the end of the file and improved output message formatting.

--- odd_Even.py
+++ odd_Even.py
@@ -1,6 +1,7 @@
 number = int(input("enter an integer number: "))
 
 if number % 2 == 0:
-    print(f"{number} is an even number")
+    print(f"{number} is an even number.")
 else:
-    print(f"{number} is odd number")
+
+    print(f"{number} is odd number.")
